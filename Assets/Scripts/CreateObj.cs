0bc3153d72c7da6784c248ca7c4040fd0f05b42b
Phantomhive123 <33385178+Phantomhive123@users.noreply.github.com>
2020-09-18 00:06:09 +0800

    做了一些UI
    现在有些问题，CreateObj脚本会执行多次！感觉还是应该分离成两个脚本

---
+++ Assets/Scripts/CreateObj.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObj : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject aimObj;
+
+    [SerializeField]
+    private GameObject preview;
+
+    private static GameObject currentAimObj;
+    private static GameObject currentPreview;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPreview != null) 
+        {
+            Vector3 Pos = Input.mousePosition;
+            currentPreview.GetComponent<RectTransform>().position = Pos;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (currentAimObj == null) return;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(aimObj, worldPos, Quaternion.identity);
+        }
+    }
+
+    public void SetCurrentObj()
+    {
+        currentAimObj = aimObj;
+        if(currentPreview)
+            currentPreview.SetActive(false);
+        currentPreview = preview;
+        if(currentPreview)
+            currentPreview.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SetPanelButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPanelButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject aimPos;
+
+    [SerializeField]
+    private GameObject panel;
+
+    [SerializeField]
+    private GameObject anotherButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetPanelPos()
+    {
+        panel.GetComponent<RectTransform>().localPosition = aimPos.GetComponent<RectTransform>().localPosition;
+        anotherButton.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
