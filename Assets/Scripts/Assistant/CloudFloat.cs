82a0c2a8c2d36b5a7560bf134503122d85807c74
Phantomhive123 <33385178+Phantomhive123@users.noreply.github.com>
2020-10-08 00:01:29 +0800

    整理了一下文件夹，增加了云朵浮动

---
+++ Assets/Scripts/Assistant/CloudFloat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudFloat : MonoBehaviour
+{
+    public Vector3[] aims;
+    public float speed = 2f;
+
+    private int currentIndex = 0;
+    private float moveThreshold = 0.05f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - aims[currentIndex]).magnitude < moveThreshold)
+        {
+            currentIndex = (currentIndex + 1) % aims.Length;
+        }
+        else
+            transform.Translate((aims[currentIndex] - transform.position).normalized * speed * Time.deltaTime);
+    }
+}
