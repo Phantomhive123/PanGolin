6c4b989b689a7e7298de839760b8d2eb515ab065
Lundy <949805411@qq.com>
2020-10-09 10:20:32 +0800

    增加太阳

---
+++ Assets/Scripts/Assistant/SunRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotate : MonoBehaviour
+{
+    public float rotateSpeed = 2f;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.fixedDeltaTime);
+    }
+}
